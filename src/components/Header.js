e5a9c6822c4bf8ba6165a36d0ab53ef83ee9a9e1
VinniFachini <vinniebrasil@gmail.com>
2020-11-21 21:57:29 -0300

    Full Project

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css'
+
+export default ({black}) => {
+
+    
+
+
+    let userPicture = 'https://occ-0-1942-1567.1.nflxso.net/dnm/api/v6/0RO1pLmU93-gdXvuxd_iYjzPqkc/AAAAFFgUUkQ31RpI8qQH98cRi6W8sLIqiNg9-8AbDIC3T1I4J1ekJXDt3ypRYEBfyWjprkK3nCB2EXU-izg0Ho27hGOzKw.png?r=f08'
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png" alt="Netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src={`${userPicture}`} alt="Usuário" />
+                </a>
+            </div>
+        </header>
+    )
+}
